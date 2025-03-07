6eb2a6756bf4fbd99bd9bc080fa07b209dd7e36b
EnricoCiccolini <piroroma90@gmail.com>
2025-03-07 14:05:42 +0100

    feat: create objeck array

---
+++ script.js
@@ -0,0 +1,28 @@
+//Creare un array di oggetti:
+//Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
+//Stampare a schermo la bici con peso minore.
+
+
+
+
+const cicles = [
+    {   model : 'gold',
+        weight : 1000
+    },
+    {   model : 'silver',
+        weight : 1200
+    },
+    {   model : 'wood',
+        weight : 600
+    },
+    {   model : 'bronze',
+        weight : 700
+    },
+    {   model : 'paper',
+        weight : 10
+    }
+ ]
+
+ console.log(cicles)
+
+
